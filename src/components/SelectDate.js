4ceafff620d1fb5b078a0e59882f408ef55aeda9
bstojanovski <bojan.stojanovski@comeon.com>
2020-04-12 19:01:09 +0200

    Fixed state issue; Formatting date variable using moment at the right place

--- src/components/SelectDate.js
+++ src/components/SelectDate.js
@@ -4,29 +4,14 @@ import DatePicker from 'react-datepicker';
 import "react-datepicker/dist/react-datepicker.css";
 
 class SelectDate extends React.Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            startDate: moment().toDate()
-        }
-        this.handleChange = this.handleChange.bind(this);
-    }
-
-    handleChange(date) {
-        this.setState({
-            startDate: this.props.startDate
-        });
-
-        console.log(this.state.startDate);
-
-        let formatted_date = ("0" + date.getDate()).slice(-2) + "." + ("0" + (date.getMonth()+1)).slice(-2) + "." + date.getFullYear();
-        this.props.onChange(formatted_date);
+    handleChange = date => {
+        this.props.readData(date);
     }
 
     render() {
         return (
             <DatePicker
-                selected={this.state.startDate}
+                selected={this.props.selectedDate}
                 onChange={this.handleChange}
                 dateFormat="dd.MM.YYYY"
                 filterDate={(date) => {
